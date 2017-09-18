a9f6d3f9f9d559b2b534f08b42bcb7fc7d2c203d
Pacho Torres <Pacho.Torres.Reyes@gmail.com>
2017-09-17 22:24:31 -0500

    droid-scripts

--- droid-scripts/update-droid-scripts.sh
+++ droid-scripts/update-droid-scripts.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-SCR_LOC=http://kanotix.com/files/kanotix-spitfire/build//
+SCR_LOC=https://raw.githubusercontent.com/frtorres/Kanotix_Guide/master/droid-scripts/
 SCR_PRF=droid*sh
 
-if test $(id -u) != 0; then
- echo Error: You must be root to run this script!
- exit 1
-fi
-cd /root
-for script in $SCR_PRF; do
+#if test $(id -u) != 0; then
+# echo Error: You must be root to run this script!
+# exit 1
+#fi
+
+for script in droid*sh; do
  echo -n $script...
  if wget -N $SCR_LOC$script >/dev/null 2>&1; then
   echo current version
